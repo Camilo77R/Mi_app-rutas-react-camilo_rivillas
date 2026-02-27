10f274bc30f33d3cec49443acd07d25708edd6df
Camilo77R <gobusqueda@gmail.com>
2026-02-27 09:39:59 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <Router>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <nav className="navbar">
+      <h1>Mi App</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
